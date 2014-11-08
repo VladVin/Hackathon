43421a33bf9e9fa5d07db5bc6063f3a765fe9efb
spacedmi <spacedmi@gmail.com>
2014-11-08 14:17:43 +0300

    Added UI

--- SpeechCities2.0/app/SpeechKitSample/CitiesGame.cs
+++ SpeechCities2.0/app/SpeechKitSample/CitiesGame.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Windows.Phone.Speech.Synthesis;
 using System.Windows.Input;
 
 namespace Yandex.SpeechKit.Demo
 {
     class CitiesGame
     {
-        CitiesGame()
+        private String[] Phrases = {"Привет хакатон, я умею говорить!", "Твой ход первый. Называй город", "Я начинаю",
+                                       "Ответ неверный", "Моя очередь", "Нужно назвать город на букву"};
+        private String[] LikePhrases = {"Ты молодец", "Умница", "Ты сегодня умненький", "Печеньку тебе", "Хорошо получается"};
+        public CitiesGame()
         {
             cities = new Cities();
             rnd = new Random();
@@ -19,6 +22,7 @@ namespace Yandex.SpeechKit.Demo
 
         public void NewGame()
         {
+            Say(Phrases[0]);
             machineScore = personScore = 0;
             cities.InitializeCitiesSet();
             action = (Actions)rnd.Next(0, 1);
@@ -34,21 +38,33 @@ namespace Yandex.SpeechKit.Demo
                 case Actions.Machine:
                     if (prevChar == '0')
                     {
+                        Say(Phrases[2]);
                         city = cities.citiesSet.ElementAt(rnd.Next(0, cities.citiesSet.Count - 1));
                     }
                     else
                     {
+                        Say(Phrases[4]);
                         city = cities.findRandomCityByFirstChar(prevChar);
                     }
+                    Say(city);
                     machineScore++;
                     action = Actions.Person;
                     break;
                 case Actions.Person:
                     if (prevChar == '0')
                     {
-                        // Say("Твой ход первый. Называй город");
-
+                        Say(Phrases[1]); 
+                        // Recognition here
+                        
+                    
                     }
+                    else
+                    {
+
+
+                        Say(LikePhrases[rnd.Next(0, 4)]);
+                    }
+                    personScore++;
                     break;
             }
             prevChar = city[city.Length - 1];
@@ -64,10 +80,22 @@ namespace Yandex.SpeechKit.Demo
             
         //}
 
-        //public async void Say(string str)
-        //{
+        public async void Say(string str)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
 
-        //}
+            // Query for a voice that speaks French.
+            IEnumerable<VoiceInformation> russianVoices = from voice in InstalledVoices.All
+                                                         where voice.Language == "ru-RU"
+                                                         select voice;
+
+            // Set the voice as identified by the query.
+            if (russianVoices.Count() > 0)
+            {
+                synth.SetVoice(russianVoices.ElementAt(0));
+                await synth.SpeakTextAsync(str);
+            }
+        }
 
         private MainViewModel model;
         private Random rnd;
--- SpeechCities2.0/app/SpeechKitSample/MainPage.xaml.cs
+++ SpeechCities2.0/app/SpeechKitSample/MainPage.xaml.cs
@@ -15,18 +15,52 @@ namespace Yandex.SpeechKit.Demo
     /// </summary>
     public partial class MainPage
     {
-        SpeechRecognition speechRec;
+        private SpeechRecognition speechRec;
+        private CitiesGame Game;
 
         public MainPage()
         {
             InitializeComponent();
             speechRec = new SpeechRecognition();
+            Game = new CitiesGame();
         }
 
-        private void Button_Tap(object sender, GestureEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string result = speechRec.SpeechResult();
+            Game.NewGame();
+            RecognizerView.StartRecognition("ru-RU", LanguageModel.General);
+        }
+        #region overrides
+        
+        protected override void OnNavigatedTo(NavigationEventArgs args)
+        {
+            //RecognizerView.StartRecognition("ru-RU", LanguageModel.General);
+
+            base.OnNavigatedTo(args);
         }
 
+        protected override void OnNavigatedFrom(NavigationEventArgs args)
+        {
+            RecognizerView.CancelRecognition();
+
+            base.OnNavigatedFrom(args);
+        }
+
+        #endregion
+
+        private void RecognizerViewFinished(object sender, RecognitionFinishedEventArgs args)
+        {
+            string result = args.Result;
+            if (!string.IsNullOrEmpty(result))
+            {
+                MessageBox.Show(result);
+            }
+
+        }
+
+        private void Ellipse_Tap(object sender, GestureEventArgs e)
+        {
+            RecognizerView.StartRecognition("ru-RU", LanguageModel.General);
+        }
     }
 }
